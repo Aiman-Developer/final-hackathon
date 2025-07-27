8d5c20bda5007419e45295bcec5c88dcf9f5724c
Aiman-Development <aimansiddiqui77254@gmail.com>
2025-07-27 13:06:10 +0500

    server deployment

--- server/index.js
+++ server/index.js
@@ -15,5 +15,9 @@ mongoose.connect(process.env.MONGO_URI)
 app.use('/feedback', require('./routes/feedbackRoutes'));
 app.use('/admin', require('./routes/adminRoutes'));
 
+app.get('/',(req,res) => {
+    res.send('server is working')
+})
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
